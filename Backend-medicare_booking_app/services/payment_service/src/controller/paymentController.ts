867d126166aa3a166659205ee9344b93d205fd48
Hung <nguyenthinhhung150603@gmail.com>
2025-12-13 20:04:18 +0700

    design layout total payment

--- Backend-medicare_booking_app/services/payment_service/src/controller/paymentController.ts
+++ Backend-medicare_booking_app/services/payment_service/src/controller/paymentController.ts
@@ -2,13 +2,59 @@ import { Request, Response } from "express";
 import { handleGetAllPayments } from "src/services/paymentService";
 
 const getAllPayments = async (req: Request, res: Response) => {
-  // Logic to get all payments
-  const payments = await handleGetAllPayments();
+  try {
+    const { page, pageSize } = req.query;
+    let currentPage = page ? +page : 1;
+    if (currentPage <= 0) {
+      currentPage = 1;
+    }
 
-  res.status(200).json({
-    message: "Get all payments successfully",
-    data: payments,
-  });
+    const size = Math.max(1, Math.min(100, Number(pageSize) || 10));
+
+    const { payments, totalItems } = await handleGetAllPayments(
+      currentPage,
+      parseInt(pageSize as string)
+    );
+
+    const pages = Math.max(1, Math.ceil(totalItems / size));
+
+    if (!payments) {
+      res.status(200).json({
+        success: true,
+        message: "Không có  thanh toán nào trong trang này",
+        data: {
+          meta: {
+            currentPage: currentPage,
+            pageSize: parseInt(pageSize as string),
+            pages: pages,
+            total: totalItems,
+          },
+          result: [],
+        },
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      length: totalItems,
+      message: "Get all payments successfully",
+      data: {
+        meta: {
+          currentPage: currentPage,
+          pageSize: parseInt(pageSize as string),
+          pages: pages,
+          total: totalItems,
+        },
+        result: payments,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
 };
 
 export { getAllPayments };
--- Backend-medicare_booking_app/services/payment_service/src/services/paymentService.ts
+++ Backend-medicare_booking_app/services/payment_service/src/services/paymentService.ts
@@ -1,11 +1,21 @@
 import { prisma } from "src/config/client";
 
-const handleGetAllPayments = async () => {
+const handleGetAllPayments = async (page: number, pageSize: number) => {
+  const skip = (page - 1) * pageSize;
   // Logic to get all payments
-  const payments = await prisma.payment.findMany();
-  console.log("Payments:", payments);
+  //   const payments = await prisma.payment.findMany();
 
-  return payments;
+  const [payments, totalItems] = await Promise.all([
+    prisma.payment.findMany({
+      skip: skip,
+      take: pageSize,
+    }),
+    prisma.payment.count(),
+  ]);
+
+  const results = { payments, totalItems };
+
+  return results;
 };
 
 export { handleGetAllPayments };
